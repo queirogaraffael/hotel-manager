7b8661c8ab23f44d52069c7e9893a71b9c78fe29
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-04-23 14:28:24 -0300

    chore: adicionar perfis de execução

--- gerenciador-hotel/src/main/java/com/example/gerenciador/hotel/GerenciadorHotelSpringApplication.java
+++ gerenciador-hotel/src/main/java/com/example/gerenciador/hotel/GerenciadorHotelSpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GerenciadorHotelSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GerenciadorHotelSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorHotelSpringApplication.class, args);
+    }
 
 }
